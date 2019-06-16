bd4620ad07a637cfb184f8e520fdbaa03ff8f0c5
Dmitriy Valickiy <valickiy.dmitriy@gmail.com>
2019-06-16 23:53:06 +0600

    First commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const { getSecret } = require('./secrets');
+
+
+mongoose.Promise = global.Promise;
+mongoose
+    .connect(
+        getSecret('dbUri'),
+        { useNewUrlParser: true }
+    )
+    .then(
+        () => {
+            console.log('Connected to mongoDB');
+        },
+        (err) => console.log('Error connecting to mongoDB', err)
+    );
+const app = express();
+const port = process.env.PORT || 3000;
+
+//sets up the middleware for parsing the bodies and cookies off of the requests
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+module.exports = { app };
